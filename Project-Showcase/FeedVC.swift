22ad7792670f68b6d76c83c70da19aa09472b698
Nicholas Ivanecky <ivanecky844@hotmail.com>
2016-02-08 17:08:08 -0500

    Parsing Firebase data

--- Project-Showcase/FeedVC.swift
+++ Project-Showcase/FeedVC.swift
@@ -7,10 +7,12 @@
 //
 
 import UIKit
+import Firebase
 
 class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var tableview: UITableView!
+    var posts = [Post]()
     
     
     override func viewDidLoad() {
@@ -18,9 +20,28 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         tableview.delegate = self
         tableview.dataSource = self
         
+        
+        //DOWNLOAD DATA FROM FIREBASE
+        
         //update posts right away
         DataService.ds.REF_POSTS.observeEventType(.Value, withBlock: { snapshot in
            print(snapshot.value)
+            
+            //clear out when
+            self.posts = []
+            
+            if let snapshots = snapshot.children.allObjects as? [FDataSnapshot] {
+                
+                for snap in snapshots {
+                    print("SNAP: \(snap)")
+                    
+                    if let postDict = snap.value as? Dictionary<String, AnyObject> {
+                        let key = snap.key
+                        let post = Post(postkey: key, dictionary: postDict)
+                        self.posts.append(post)
+                    }
+                }
+            }
            self.tableview.reloadData()
         })
     }
@@ -30,10 +51,14 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return posts.count
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let post = posts[indexPath.row]
+        print(post.postDescription)
+        
         return tableview.dequeueReusableCellWithIdentifier("PostCell") as! PostCell
     }
 
---
+++ Project-Showcase/Post.swift
@@ -0,0 +1,58 @@
+//
+//  Post.swift
+//  Project-Showcase
+//
+//  Created by Nick on 2016-02-08.
+//  Copyright © 2016 Nicholas Ivanecky. All rights reserved.
+//
+
+import Foundation
+
+class Post {
+    private var _postDescription: String!
+    private var _imageUrl: String?
+    private var _likes: Int!
+    private var _username: String!
+    private var _postKey: String!
+    
+    var postDescription: String {
+        return _postDescription
+    }
+    
+    var imageUrl: String? {
+        return _imageUrl
+    }
+    
+    var likes: Int {
+        return _likes
+    }
+    
+    var username: String {
+        return _username
+    }
+    
+    
+    init(description: String, imageUrl: String?, username: String) {
+        self._postDescription = description
+        self._imageUrl = imageUrl
+        self._username = username
+    }
+    
+    init(postkey: String, dictionary: Dictionary<String, AnyObject>) {
+        self._postKey = postkey
+        
+        if let likes = dictionary["likes"] as? Int {
+            self._likes = likes
+        }
+        
+        if let imgUrl = dictionary["imageUrl"] as? String {
+            self._imageUrl = imgUrl
+        }
+        
+        if let desc = dictionary["description"] as? String {
+            self._postDescription = desc
+        }
+        
+    }
+    
+}
